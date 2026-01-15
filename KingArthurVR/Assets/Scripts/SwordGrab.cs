8afdc3876aa8744f07934fa6975d657c9f688ec4
C342 <mbuzurin2024@elam.co.uk>
2026-01-15 12:39:52 +0000

    story/sword scrpt

---
+++ KingArthurVR/Assets/Scripts/SwordGrab.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(XRGrabInteractable))]
+[RequireComponent(typeof(ConfigurableJoint))]
+public class SwordInStone : MonoBehaviour
+{
+    private XRGrabInteractable grab;
+    private ConfigurableJoint joint;
+
+    void Awake()
+    {
+        grab = GetComponent<XRGrabInteractable>();
+        joint = GetComponent<ConfigurableJoint>();
+
+        grab.selectEntered.AddListener(OnGrab);
+        grab.selectExited.AddListener(OnRelease);
+
+        LockJoint();
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        UnlockJoint();
+    }
+
+    private void OnRelease(SelectExitEventArgs args)
+    {
+        LockJoint();
+    }
+
+    private void UnlockJoint()
+    {
+        joint.xMotion = ConfigurableJointMotion.Locked;
+        joint.yMotion = ConfigurableJointMotion.Limited;
+        joint.zMotion = ConfigurableJointMotion.Locked;
+    }
+
+    private void LockJoint()
+    {
+        joint.xMotion = ConfigurableJointMotion.Locked;
+        joint.yMotion = ConfigurableJointMotion.Locked;
+        joint.zMotion = ConfigurableJointMotion.Locked;
+    }
+}
