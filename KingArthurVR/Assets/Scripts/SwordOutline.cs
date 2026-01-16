1f233ecf55e1152d16d7809a97380885d5b02cad
C342 <mbuzurin2024@elam.co.uk>
2026-01-16 15:36:45 +0000

    outline start

---
+++ KingArthurVR/Assets/Scripts/SwordOutline.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SwordOutline : MonoBehaviour
+{
+    public Transform Sword;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
